5ad179933cd3f96fac69bfe0f020667ff73f5778
vivek451 <vivekmogalla9876@gmail.com>
2024-01-27 20:07:48 +0530

    Configure Django Project

---
+++ OpenPaymentsAnalytics/apps/openpaymentsapp/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class OpenpaymentsappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    # name = 'openpaymentsapp'
+    name = 'apps.openpaymentsapp'
